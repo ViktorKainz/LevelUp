4103609941b537eed76bcd2ee5a7e675b426b923
Florian Angerer <florian.angerer.2001@gmail.com>
2020-10-01 12:52:17 +0200

    Ruby texture with bug

--- src/main/java/at/htlkaindorf/levelup/ModItems.java
+++ src/main/java/at/htlkaindorf/levelup/ModItems.java
@@ -1,19 +1,23 @@
 package at.htlkaindorf.levelup;
 
-import at.htlkaindorf.levelup.items.Ruby;
+import at.htlkaindorf.levelup.items.ItemBase;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraftforge.registries.IForgeRegistry;
 
 public class ModItems {
 
+    public static ItemBase ruby = new ItemBase("Ruby").setCreativeTab(CreativeTabs.MATERIALS);
+
+
     public static void register(IForgeRegistry<Item> registry) {
         registry.registerAll(
-                Ruby.ruby
+                ruby
         );
     }
 
     public static void registerModels() {
-        Ruby.ruby.registerItemModel();
+        ruby.registerItemModel();
     }
 
 }
