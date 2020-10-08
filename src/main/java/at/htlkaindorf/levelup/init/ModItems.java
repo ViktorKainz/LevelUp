b96d0d8ba3d812c87ea325f93fb51b785d83af65
Florian Angerer <florian.angerer.2001@gmail.com>
2020-10-08 14:16:42 +0200

    added ruby_sword

--- src/main/java/at/htlkaindorf/levelup/init/ModItems.java
+++ src/main/java/at/htlkaindorf/levelup/init/ModItems.java
@@ -2,6 +2,8 @@ package at.htlkaindorf.levelup.init;
 
 import at.htlkaindorf.levelup.LevelUp;
 import at.htlkaindorf.levelup.items.Ruby;
+import at.htlkaindorf.levelup.items.RubySword;
+import at.htlkaindorf.levelup.materials.Materials;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.event.ModelRegistryEvent;
@@ -13,21 +15,25 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 @Mod.EventBusSubscriber(modid = LevelUp.MOD_ID)
 public class ModItems {
     public static Item Ruby;
+    public static Item RubySword;
 
     public static void init() {
         Ruby = new Ruby("ruby");
+        RubySword = new RubySword(Materials.rubyToolMatMATERIAL, "ruby_sword");
     }
 
     @SubscribeEvent
     public static void registerItems(RegistryEvent.Register<Item> event) {
         event.getRegistry().registerAll(
-                Ruby
+                Ruby,
+                RubySword
         );
     }
 
     @SubscribeEvent
     public static void registerRenders(ModelRegistryEvent event) {
         registerRender(Ruby);
+        registerRender(RubySword);
     }
 
     private static void registerRender(Item item) {
---
+++ src/main/java/at/htlkaindorf/levelup/materials/Materials.java
@@ -0,0 +1,9 @@
+package at.htlkaindorf.levelup.materials;
+
+import net.minecraft.item.Item;
+import net.minecraftforge.common.util.EnumHelper;
+
+public class Materials {
+    public static final Item.ToolMaterial rubyToolMatMATERIAL = EnumHelper.addToolMaterial("RUBY", 3, 2000, 12.0F, 4F, 10);
+
+}
