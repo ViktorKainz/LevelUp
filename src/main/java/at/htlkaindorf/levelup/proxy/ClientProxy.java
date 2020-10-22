71715346e831c0c75203a3969e6b82ac74403f6f
Florian Angerer <florian.angerer.2001@gmail.com>
2020-10-22 11:41:54 +0200

    Added KeyBindings

--- src/main/java/at/htlkaindorf/levelup/proxy/ClientProxy.java
+++ src/main/java/at/htlkaindorf/levelup/proxy/ClientProxy.java
@@ -1,19 +1,39 @@
 package at.htlkaindorf.levelup.proxy;
 
+import net.minecraft.client.settings.KeyBinding;
 import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
+import org.lwjgl.input.Keyboard;
 
 @Mod.EventBusSubscriber(Side.CLIENT)
 public class ClientProxy extends CommonProxy {
+
+    public static KeyBinding[] keyBindings;
 
     @Override
     public void preinit(FMLPreInitializationEvent event) {
         super.preinit(event);
     }
 
+    @Override
+    public void init(FMLInitializationEvent event) {
+        super.init(event);
+        keyBindings = new KeyBinding[1];
+
+        keyBindings[0] = new KeyBinding("Open Interface", Keyboard.KEY_B, "LevelUP");
+
+        for (KeyBinding keyBinding : keyBindings) {
+            ClientRegistry.registerKeyBinding(keyBinding);
+        }
+    }
+
     @SubscribeEvent
-    public static void registerRenders(ModelRegistryEvent event) {}
+    public static void registerRenders(ModelRegistryEvent event) {
+    }
+    
 }
