51d5fef26238644ed8ed5e85a1b4d4bcfe2359d5
Viktor Kainz <viktor.kainz@gmail.com>
2020-10-26 19:46:53 +0100

    Fixed config file

--- src/main/java/at/htlkaindorf/levelup/capability/ModConfig.java
+++ src/main/java/at/htlkaindorf/levelup/capability/ModConfig.java
@@ -11,34 +11,32 @@ import java.util.List;
 @Config(modid = LevelUp.MODID, name = "levelup")
 public class ModConfig {
     @Config.Comment({
-            "Group attributes are split by a ',' and Groups are split by a new line",
+            "Group attributes are split by a , and Groups are split by a new line",
             "Types: Mining, Crafting, Farming, Building, Fighting, Enchanting",
-            "Name,Type,Level",
             "Name,Type,Level"
     })
-    public static String groups = "Stone Tools,Crafting,1\n" +
-            "Iron Tools,Crafting,2";
+    public static String[] groups = {
+            "Stone Tools,Crafting,1",
+            "Iron Tools,Crafting,2"};
     @Config.Comment({
             "Line starts with the group name.",
-            "The resource locations of the Items are split by a ','",
+            "The resource locations of the Items are split by a ,",
             "and Groups are split by a new line",
-            "Name,resource location,resource location,...",
             "Name,resource location,resource location,..."
     })
-    public static String groupsContent = "Stone Tools,minecraft:stone_axe,minecraft:stone_pickaxe,minecraft:stone_hoe,minecraft:stone_shovel\n" +
-            "Iron Tools,minecraft:iron_axe,minecraft:iron_pickaxe,minecraft:iron_hoe,minecraft:iron_shovel";
+    public static String[] groupsContent = {
+            "Stone Tools,minecraft:stone_axe,minecraft:stone_pickaxe,minecraft:stone_hoe,minecraft:stone_shovel",
+            "Iron Tools,minecraft:iron_axe,minecraft:iron_pickaxe,minecraft:iron_hoe,minecraft:iron_shovel"};
 
     public static void init() {
         List<Group> groupList = new ArrayList<>();
-        String[] groupArray = groups.split("\n");
-        String[] groupContentArray = groupsContent.split("\n");
-        for (int i = 0; i < groupArray.length; i++) {
-            String[] group = groupArray[i].split(",");
+        for (int i = 0; i < groups.length; i++) {
+            String[] group = groups[i].split(",");
             groupList.add(new Group(group[0], ExperienceType.valueOf(group[1]), Integer.parseInt(group[2])));
         }
-        for (int i = 0; i < groupContentArray.length; i++) {
+        for (int i = 0; i < groupsContent.length; i++) {
             List<ResourceLocation> resources = new ArrayList<>();
-            String[] group = groupContentArray[i].split(",");
+            String[] group = groupsContent[i].split(",");
             for (int j = 1; j < group.length; j++) {
                 resources.add(new ResourceLocation(group[j]));
             }
